eb844ae3dbcc350fa013a867c7d42c6ea3bbb8b0
NITISH084 <imnitishkumar04@gmail.com>
2024-07-05 02:16:23 +0530

    frontend bugs

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
 export default {
   content: [
     "./index.html",
@@ -8,9 +9,8 @@ export default {
     extend: {
       fontFamily: {
         playfair: ['"Playfair Display"', 'serif'],
-        nunito:[ '"Nunito"', 'sans-serif'],
+        nunito: ['"Nunito"', 'sans-serif'],
       },
-      
       animation: {
         'spin-slow': 'spin 3s linear infinite',
       },
@@ -21,11 +21,12 @@ export default {
         'custom-gradient1': 'linear-gradient(to bottom, rgba(0, 0, 0, 3.8) 0%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 3.8) 100%)',
         'custom-gradient2': 'linear-gradient(to right, rgba(0, 0, 0, 1.8) 0%, rgba(0, 0, 0, 0.2) 100%)',
         'custom-gradient3': 'linear-gradient(to bottom, rgba(0, 0, 0, 1.8) 0%, rgba(0, 0, 0, 0.2) 100%)',
-        'custom-gradient4': 'linear-gradient(to bottom, rgba(0, 0, 0, 1.5) 0%,  rgba(0, 0, 0, 0.6)40%,rgba(0, 0, 0, 0.8) 100%)',
+        'custom-gradient4': 'linear-gradient(to bottom, rgba(0, 0, 0, 1.5) 0%, rgba(0, 0, 0, 0.6) 40%, rgba(0, 0, 0, 0.8) 100%)',
       },
     },
   },
   plugins: [
-    require('tailwind-scrollbar-hide')
+    scrollbarHide(),
+  
   ],
-}
+};
