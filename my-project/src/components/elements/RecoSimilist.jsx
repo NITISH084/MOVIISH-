4a00415230a2f47764623c5b7e3c1736553f7a50
NITISH084 <imnitishkumar04@gmail.com>
2024-07-05 04:02:57 +0530

    css mistake

--- my-project/src/components/elements/RecoSimilist.jsx
+++ my-project/src/components/elements/RecoSimilist.jsx
@@ -27,7 +27,7 @@ const RecoSimilist = () => {
   useRecommendation();
   useSimilar();
   return (
-    <div className="bg-black overflow-hidden h-auto">
+    <div className="bg-black overflow-hidden h-fit min-h-[100vh]">
       <div className="flex  flex-col relative ">
         <div className="px-8 font-playfair">
           <p
