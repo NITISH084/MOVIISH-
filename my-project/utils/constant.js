6f133304a3ac7fd6251b59f0d81c3fd50d0c5416
NITISH KUMAR <imnitishkumar04@gmail.com>
2024-07-19 13:49:23 +0530

    Update constant.js

--- my-project/utils/constant.js
+++ my-project/utils/constant.js
@@ -1,4 +1,4 @@
-export const endpoint_api ="https://moviish.vercel.app/api/v1/user";
+export const endpoint_api ="https://moviish-backend1.onrender.com/api/v1/user";
  export const options = {
     method: 'GET',
     headers: {
