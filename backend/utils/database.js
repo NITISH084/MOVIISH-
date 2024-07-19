06b90f7f0eac49f3708fe45e10f40df7dec0b263
NITISH KUMAR <imnitishkumar04@gmail.com>
2024-07-19 13:28:05 +0530

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const databaseConnection = async() => {
   try{
- await mongoose.connect(process.env.Mongo_url).then(() => {
+ await mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Database connected successfully');
   })}
   catch(error){
